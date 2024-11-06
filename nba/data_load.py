88737ba15cb1586d18af731a7f774ca5615c28cf
mz537 <mz537@cam.ac.uk>
2024-11-06 13:58:42 +0000

    finished the first question of pathlib and verified functionality by adding pring(df.head())

--- nba/data_load.py
+++ nba/data_load.py
@@ -8,6 +8,7 @@ def load_csv(data_path: Path) -> pd.DataFrame:
 
 if __name__ == "__main__":
     # TODO: fix this data path using the pathlib library
-    data_path = "/Users/adrianochs/all_my_great_data/game.csv"
+    data_path =Path(__file__).parent.parent / "data" / "game.csv" #changed the code based on the notes
     df = load_csv(data_path)
     print(data_path)
+    print(df.head) #added this to show that the data_path code actually worked
